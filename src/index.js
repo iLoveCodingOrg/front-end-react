5cce4faa6a0cdbf331ab8181917f44ce0fde2e3d
Aziz Ali <aziz@ilovecoding.org>
2018-05-21 18:22:46 -0500

    ScrollToTop

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import {
 } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-
 import store from './_app/store'
 import Wrap from './Wrap'
+
+import ScrollToTop from './ScrollToTop'
 import { Home } from './Home'
 import {
     View as LessonView,
@@ -37,61 +38,63 @@ function renderApp(){
     render(
         <Provider store={store}>
             <BrowserRouter>
-                <Switch>
-                    <Route
-                        exact
-                        strict
-                        path="/login"
-                        component={Login} />
-                    <Wrap>
-                        <Switch>
-                            <Route
-                                exact
-                                path="/"
-                                component={Home} />
-            
-                            <Route
-                                exact
-                                strict
-                                path="/lessons"
-                                component={LessonList} />
-                            <Route
-                                exact
-                                strict
-                                path="/lessons/:slug"
-                                component={LessonView} />
-                            <Route
-                                exact
-                                strict
-                                path="/lessons/:slug/edit"
-                                component={LessonEdit} />
-            
-                            <Route
-                                strict
-                                path="/courses"
-                                component={CourseRoutes} />
-            
-                            <Route
-                                exact
-                                strict
-                                path="/pages"
-                                component={PageList} />
-                            <Route
-                                exact
-                                strict
-                                path="/pages/:slug"
-                                component={PageView} />
-                            <Route
-                                exact
-                                strict
-                                path="/pages/:slug/edit"
-                                component={PageEdit} />
+                <ScrollToTop>
+                    <Switch>
+                        <Route
+                            exact
+                            strict
+                            path="/login"
+                            component={Login} />
+                        <Wrap>
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/"
+                                    component={Home} />
+                
+                                <Route
+                                    exact
+                                    strict
+                                    path="/lessons"
+                                    component={LessonList} />
+                                <Route
+                                    exact
+                                    strict
+                                    path="/lessons/:slug"
+                                    component={LessonView} />
+                                <Route
+                                    exact
+                                    strict
+                                    path="/lessons/:slug/edit"
+                                    component={LessonEdit} />
+                
+                                <Route
+                                    strict
+                                    path="/courses"
+                                    component={CourseRoutes} />
+                
+                                <Route
+                                    exact
+                                    strict
+                                    path="/pages"
+                                    component={PageList} />
+                                <Route
+                                    exact
+                                    strict
+                                    path="/pages/:slug"
+                                    component={PageView} />
+                                <Route
+                                    exact
+                                    strict
+                                    path="/pages/:slug/edit"
+                                    component={PageEdit} />
 
-                            <Route
-                                component={NotFound} />
-                        </Switch>
-                    </Wrap>
-                </Switch>
+                                <Route
+                                    component={NotFound} />
+                            </Switch>
+                        </Wrap>
+                    </Switch>
+                </ScrollToTop>
             </BrowserRouter>
         </Provider>
         ,
