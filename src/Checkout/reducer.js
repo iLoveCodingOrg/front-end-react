1dc79a0c51b0eca80f8fb2247848e0c871d78754
Aziz Ali <aziz@ilovecoding.org>
2018-05-22 04:45:03 -0500

    removed unfinished components

--- src/Checkout/reducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-    show: true
-}
-
-export default function(state = initialState, action) {
-    state.show = !state.show
-    var newState =  Object.assign({}, state)
-    return newState
-}
--- src/Dashboard/index.js
+++
@@ -1,5 +0,0 @@
-import * as actionCreators from './actionCreators'
-import reducer from './reducer'
-import Home from './components/HomeContainer'
-
-export { reducer, actionCreators, Home }
