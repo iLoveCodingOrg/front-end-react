8526d8095d454c2a5d891c488799d21ac320cab0
Aziz Ali <aziz@ilovecoding.org>
2018-05-26 17:25:57 -0500

    UserForm made into class-based component

--- src/Checkout/components/UserForm.js
+++ src/Checkout/components/UserForm.js
@@ -1,47 +1,64 @@
 import React from 'react'
 
-export default function(props) {
-    return (
-        <div>
-            <h4 className="mb-3">Contact Information</h4>
-            <form className="needs-validation" noValidate>
-                <div className="row">
-                    <div className="col-md-6 mb-3">
-                        <label htmlFor="firstName">First name</label>
-                        <input type="text"
-                            className="form-control"
-                            id="firstName"
-                            placeholder="First Name"
-                            required />
-                        <div className="invalid-feedback">
-                            Valid first name is required.
-                        </div>
-                    </div>
-                    <div className="col-md-6 mb-3">
-                        <label htmlFor="lastName">Last name</label>
-                        <input type="text"
-                            className="form-control"
-                            id="lastName"
-                            placeholder="Last Name"
-                            required />
-                        <div className="invalid-feedback">
-                            Valid last name is required.
-                        </div>
-                    </div>
-                </div>
+class UserForm extends React.Component{
+    constructor(props){
+        super(props)
 
-                <div className="mb-3">
-                    <label htmlFor="email">Email</label>
-                    <input type="email"
-                        className="form-control"
-                        id="email"
-                        placeholder="your@email.com"
-                    />
-                    <div className="invalid-feedback">
-                        Please enter a valid email address for shipping updates.
+        this.validate = this.validate.bind(this)
+    }
+    
+    validate(email, password) {
+        return {
+          email: email.length === 0,
+          password: password.length === 0,
+        }
+      }
+
+    render(){
+        return (
+            <div>
+                <h4 className="mb-3">Contact Information</h4>
+                <form className="needs-validation" noValidate>
+                    <div className="row">
+                        <div className="col-md-6 mb-3">
+                            <label htmlFor="firstName">First name</label>
+                            <input type="text"
+                                className="form-control"
+                                id="firstName"
+                                placeholder="First Name"
+                                required />
+                            <div className="invalid-feedback">
+                                Valid first name is required.
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <label htmlFor="lastName">Last name</label>
+                            <input type="text"
+                                className="form-control"
+                                id="lastName"
+                                placeholder="Last Name"
+                                required />
+                            <div className="invalid-feedback">
+                                Valid last name is required.
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    )
+
+                    <div className="mb-3">
+                        <label htmlFor="email">Email</label>
+                        <input type="email"
+                            className="form-control"
+                            id="email"
+                            placeholder="your@email.com"
+                        />
+                        <div className="invalid-feedback">
+                            Please enter a valid email address for shipping updates.
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default UserForm
