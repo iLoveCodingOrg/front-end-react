ca1194e3ea3d989084fc6542ed4663901b6fe774
Aziz Ali <aziz@ilovecoding.org>
2018-05-27 14:54:31 -0500

    Show relevant price

--- src/Checkout/components/Due.js
+++ src/Checkout/components/Due.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-export default function (props) {
+export default function ({ price, billingDuration }) {
+
     return (
         <div className="card border p-3 d-flex flex-row justify-content-between mb-3">
             <div className="h4 mb-0">Due Today</div>
-            <div className="h4 text-success mb-0">$599</div>
+            <div className="h4 text-success mb-0">${price}{getBillingDurationText(billingDuration)}</div>
         </div>
     )
+}
+
+function getBillingDurationText(billingDuration){
+    switch(billingDuration){
+        case 'month':
+            return '/month'
+        case 'year':
+            return '/year'
+        default:
+            return ''
+    }
 }
