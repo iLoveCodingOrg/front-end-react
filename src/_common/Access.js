dca94daef1ad3ab73031781036481f4e5f048212
Aziz Ali <aziz@ilovecoding.org>
2018-10-30 13:23:35 -0500

    FREE label will show regardless of login state

--- src/_common/Access.js
+++ src/_common/Access.js
@@ -1,38 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { selectors } from '../_user/'
 
-class Access extends React.Component{
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return (
-            <span>
-                {
-                    this.props.isFree?
-                        this.props.isLoggedIn?
-                        null
-                        :
-                        <span className="badge badge-pill badge-dark mr-3">FREE</span>
-                    :
-                    <span className="badge badge-pill badge-success mr-3">PRO</span>
-                }
-            </span>
-        )
-    }
+export default function Access({isFree = true}){
+    return (
+        <span>
+            {
+                isFree?
+                <span className="badge badge-pill badge-dark mr-3">FREE</span>
+                :
+                <span className="badge badge-pill badge-success mr-3">PRO</span>
+            }
+        </span>
+    )
 }
-
-Access.propTypes = {
-    isFree: PropTypes.bool.isRequired,
-    isLoggedIn: PropTypes.bool.isRequired
-}
-
-function mapStateToProps(state){
-    return {
-        isLoggedIn: selectors.isLoggedIn(state)
-    }
-}
-export default connect(mapStateToProps)(Access)
