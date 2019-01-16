c9c54b907c0744a0957ef99362546de5126fbce6
Aziz Ali <aziz@ilovecoding.org>
2019-01-15 19:41:26 -0600

    FAQs Component with content created

--- src/Pricing/components/PricingPage.js
+++ src/Pricing/components/PricingPage.js
@@ -19,14 +19,27 @@ class Pricing extends React.Component{
                 <Helmet>
                     <title>Pricing - iLoveCoding</title>
                 </Helmet>
+                
+                BIG GOAL YOU WILL ACHIEVE
+                Or
+                Big thing about iLoveCoding
+
+                Get this without that.
+
+                Price
+                What you get
+                Compare Price
+                Guarantee
+                Common questions
+
                 <p className="alert alert-warning lead text-center">
                     <strong>Let’s be honest</strong> - Coding bootcamps are charging an arm and a leg - We give you <span className="strike">same</span> <Link to="/questions/how-better" className="u">better training</Link> for 2% of the cost.
                 </p>
                 <div className="pt-5"><Header /></div>
                 <div className="pt-3"><Price /></div>
                 <div className="py-5 bg-gray-200"><Testimonials /></div>
-                <div className="py-5"><FAQs /></div>
                 <div className="py-5"><Guarantee /></div>
+                <div className="py-5 bg-gray-200"><FAQs /></div>
             </div>
         )
     }
