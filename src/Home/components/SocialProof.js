e3fa51d682b1c3363d58453d377482a06a6fe212
Aziz Ali <aziz@ilovecoding.org>
2019-02-01 17:22:38 -0600

    january numbers updated

--- src/Home/components/SocialProof.js
+++ src/Home/components/SocialProof.js
@@ -30,7 +30,7 @@ class SocialProof extends React.Component {
     render() {
         return (
                     <div className="">
-                        <h3 className="text-center mb-4"><u>5263 students</u> learned coding from iLoveCoding last month;
+                        <h3 className="text-center mb-4"><u>5917 students</u> learned coding from iLoveCoding last month;
                         <br/> Here's what some of them had to say:</h3>
                         <PrintReviews
                             containerClasses=""
