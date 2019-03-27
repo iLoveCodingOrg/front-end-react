3e98810b18138e29b99875aaafade4e68c02493f
Aziz Ali <aziz@ilovecoding.org>
2019-03-27 18:26:21 -0500

    updated testimonials

--- src/Reviews/data.js
+++ src/Reviews/data.js
@@ -117,6 +117,123 @@ const data = [
     credit1: 'Venky',
     credit2: 'India'
   }
-
+  ,{
+    title: 'More real-world examples than any other platform',
+    body: 'The best part about iLoveCoding is that it has more real-world problems then any other options',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/richard-farr.jpg',
+    credit1: 'Richard Farr',
+    credit2: 'Online Marketing Professional'
+  }
+  ,{
+    title: 'Aziz creates a map for you so you can accomplish your goal.',
+    body: 'iLoveCoding has a unique style of teaching. Aziz doesn\'t get stuck on little things. He sets a goal in mind and writes code to accomplish that goal. That\'s very important when learning coding. You could easily spend all the time you have and learn HTML and master it but before you know it you\'ve lost a lot of precious time that you could have used to learn other languages (like JavaScript) that are mandatory to accomplish your end results. Aziz creates a map for you and in his one-on-one call he gives you a very good idea which path you need to choose to get to your goals. I\'ve learned from iLoveCoding what I need to do and learn to successfully create the project that I want to run. iLoveCoding showing me the path, Making me think outside the box. It has great tips and tricks in courses and making me go from being completely lost to having realistic goals and plans set in place. I have membership in other sites but they use different teachers for different courses. Aziz teaches you how to get rid of the clutter and not get stuck on little things. I can\'t emphasize how important that lesson was to me.  As a perfectionist I thought the more I learn about one course the more it\'s going to help me, but after watching iLoveCoding courses, I realized not to spend too much time on stuff that I can easily look up.',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/ashley-novik.png',
+    credit1: 'Ashley Novik',
+    credit2: 'Entrepreneur (with a lot of ideas)'
+  }
+  ,{
+    title: 'Aziz keeps the learning simple and easy.',
+    body: 'My time using iLoveCoding has been very good. I am able to learn new skills. Plus a lot of great information is given. And Aziz keeps the learning simple and easy as he teaches.',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/glen-t-vaci.jpg',
+    credit1: 'Glen T Vaci',
+    credit2: 'Web Freelancer, Chicago'
+  }
+  ,{
+    title: 'Aziz is very responsive and I feel like he cares for my success!',
+    body: 'Personally, I have tried many ways to learn how to code. I have tried dozens of cheap courses, and have spent endless amount of time googling, praying and hoping to find a good formula to become a software developer. Despite my efforts I could not find a clear answer to my questions until I joined ILoveCoding. Aziz is a natural teacher, very helpful, and he tries to simplify things for you with a easy and simple language. I have no coding background and I can follow his instructions clearly. With his teaching style, I am hooked to gain more knowledge and make progress. The one thing I love about iLoveCoding is that I have a clear and straightforward path of learning towards my goal of becoming a software engineer. I am making progress fast, and whenever I am stuck, I can easily reach out to Aziz in the Student Community. Aziz is very responsive and I feel like he cares for my success. With this kind of training and support, I am sure I will be finishing the Front-End section and projects, and I will be ready to get a software developer job.',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/younes-ahnouche.jpg',
+    credit1: 'Younes Ahnouche',
+    credit2: ''
+  }
+  ,{
+    title: 'iLoveCoding content goes from basic to advanced!',
+    body: 'I am glad I found iLoveCoding. The course content is great, covers from basics to advanced concepts. Aziz teaching style is unique and the ease in explaining the concepts makes the course work enjoyable. If you are into web development and looking to improve your skill set or just a beginner, this is the place for you.',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/prathima-anireddy.jpg',
+    credit1: 'Prathima Anireddy',
+    credit2: ''
+  },
+  {
+    title: 'I\'d highly recommend iLoveCoding',
+    body: 'The courses will take you through what you need to know, and then doing the tutorials puts what you\'ve learned into practical application in multiple ways. A lot of other websites miss to teach how to put your learning into practical application, instead they only giving you a small problem to solve.',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/lee-delarm.png',
+    credit1: '‎Lee Delarm',
+    credit2: 'Senior JavaScript Developer'
+  },
+  {
+    title: 'It\'s all in one place. What more can one ask for?',
+    body: 'Full-stack JavaScript developers have a nightmare finding tutorials and help all in one place. And usually, a front-end JavaScript developer won\'t be able to impart good knowledge about back-end JavaScript. But iLoveCoding has it all in one place. And the price is cheap too!',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/manit-singh.png',
+    credit1: '‎Manit Singh',
+    credit2: 'Software Engineer at Amazon'
+  },
+  {
+      title: 'I tried for 2 years without luck, and now finally It\'s clicking with iLoveCoding',
+      body: 'Aziz, Just started last week with the HTML/CSS. You have a simple way of teaching HTML/CSS. Over the past two years, I have tried to grasp HTML/CSS from several books with no luck. I am almost done with the HTML/CSS training. Looking forward to learning JavaScript and working on the projects.',
+      thumbSrc: '//a.ilovecoding.org/img/_reviews/benson-mui.jpg',
+      credit1: '‎Benson Mui‎',
+      credit2: ''
+  }
+  ,{
+    title: '',
+    body: 'Aziz has a practical approach. Learn something and try it. I gave some of the videos a try and just kept going. The personal contact with Aziz through email has been awesome. He\'s very encouraging and is always working to enhance his learning tool. The practical nature of the courses. I can watch and apply. Aziz isn\'t making poor assumptions and skipping details. He explains things so I can understand.',
+    thumbSrc: '',
+    credit1: 'Josh Wolpinsky',
+    credit2: ''
+  }
+  ,{
+    title: 'helped me understand an easy and definite learning path for both Front-End and Full-Stack technologies',
+    body: 'I was learning Front-End technologies from every possible online course (codeacademy, treehouse, codeschool, and udemy. But, none of them maintained the balance between content and its application. I was searching for a tutor who can teach me basics  while building something with that knowledge. One of my developer friends told me about Aziz and his website iLoveCoding. iLoveCoding provides a definite path for both front-end and back-end technologies, besides providing hands-on experience using the basics of those technologies and Aziz organized his content in such ways that even those who do not have computer science background can easily understand and build something from day one. How much information you know is not important, how you are going to use it to produce something is important. iLoveCoding helped me understand an easy and definite learning path for both front-end and back-end technologies. iLoveCoding provides a definite path for both front-end and back-end technologies, besides providing hands-on experience using the basics of those technologies and Aziz organized his content in such ways that even those who do not computer science background can easily understand and build something from day one. It\'s all about Basics... Basics... Basics... Yes... Basics',
+    thumbSrc: '//a.ilovecoding.org/img/_reviews/venky.jpg',
+    credit1: 'Venky',
+    credit2: ''
+  }
+  ,{
+    title: 'I like your teaching style.',
+    body: 'I had a good experience on iLoveCoding. I learned several things (I think node was the best one for me; I still use the lesson from the scraper from time to time.) and I like your teaching style.',
+    thumbSrc: '',
+    credit1: 'Shomari Haynes',
+    credit2: ''
+  }
+  ,{
+    title: 'Highly Recommended',
+    body: 'I highly recommend iLoveCoding to my friends, and co-workers.',
+    thumbSrc: '',
+    credit1: 'Shëndrit Katana',
+    credit2: ''
+  }  ,{
+    title: '',
+    body: 'iLoveCoding has helped me solidify my foundation and learn the code. It has helped me very much. Happy!',
+    thumbSrc: '',
+    credit1: 'Saketh',
+    credit2: ''
+  }
+  ,{
+    title: 'I can follow the instructions and I don\'t feel overwhelmed.',
+    body: 'The instructions are very easy to understand. The best part is that I can follow the instructions and I don\'t feel overwhelmed.',
+    thumbSrc: '',
+    credit1: 'Raman saxena',
+    credit2: ''
+  }
+  ,{
+    title: 'Explanations are crystal clear, which is a PLUS',
+    body: 'I\'m self-motivated to become a great web developer and iLoveCoding is the resource I picked to learn my dream career. The information is really good as well as the way to explain how to code is crystal clear which is a + plus. HTML and CSS basics are really important and helped me to make a couple of nice static webpages. I have been able to put what I learned in practice and created two awesome (there is room for improvement) web pages, that increases my motivation to keep learning. I\'ve seen a few tutorials about web development. With iLoveCoding I am able to understand the concepts very easily and I don\'t see that in other tutorials out there',
+    thumbSrc: '',
+    credit1: 'Alejandro',
+    credit2: ''
+  }
+  ,{
+    title: 'Making constant progress!',
+    body: 'I picked iLoveCoding since its so good. It\'s good to learn and make constant progress,',
+    thumbSrc: '',
+    credit1: 'Jaydip Patel',
+    credit2: ''
+  }
+  ,{
+    title: 'Aziz\'s explanation are quite compact and vivid',
+    body: 'I\'m in third year of my under-graduate studies and I find myself way too dull in programming. Every time I start any new online tutorial, it becomes boring and I lose my motivation. Actually I didn\'t want to go for mobile app development or such, I wanted to go for web designing, but the University routine is so much hectic to carry on tutorials other than university lectures. Since I have started learning HTML and CSS from iLoveCoding, I am enjoying every bit of it. I am looking forward to excelling my skills with every lesson I do. Aziz\'s explanation are quite compact and vivid.',
+    thumbSrc: '',
+    credit1: 'Mafaza Syed',
+    credit2: ''
+  }
 ]
 export default data
