bda06859ea37b743131d763f43540682a1a3ba46
Aziz Ali <aziz@ilovecoding.org>
2019-04-01 13:01:31 -0500

    Social Proofs upgraded + minor aesthetic changes

--- src/Footer/components/Copyrights.js
+++ src/Footer/components/Copyrights.js
@@ -4,8 +4,24 @@ import { CURRENT_YEAR } from '../../_app/constants'
 
 export default ({ className }) => (
     <small className={`d-block mt-5 mb-3 text-muted ${className}`}>
-        © 2014 - {CURRENT_YEAR} iLoveCoding Inc. ⚡ by drive and commitment 
+        © 2014 - {CURRENT_YEAR} iLoveCoding Inc. ⚡ by drive and commitment.
         <Link to="/pages/privacy" className="pl-1 text-muted"><u>Privacy</u></Link>
         <Link to="/pages/terms" className="pl-1 text-muted"><u>Terms</u></Link>
+        <div>Have a great {getDay()} 👌</div>
     </small>
 )
+
+function getDay(){
+    const weekday = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+
+    const day = (new Date()).getDay()
+    return weekday[day]
+}
--- src/Footer/components/Footer.js
+++ src/Footer/components/Footer.js
@@ -8,28 +8,28 @@ export default () => (
         <div className="align-self-center container">
             <div className="my-2">
                 <span className="strong pr-1">Students</span>
-                <Link to="/curriculum" className="text-muted u px-1">Full Curriculum</Link>
-                <Link to="/pages/resources" className="text-muted u px-1">Tools &amp; Resources</Link>
-                <Link to="/reviews" className="text-muted u px-1">Reviews</Link>
+                <Link to="/curriculum" className="text-muted u px-2">Full Curriculum</Link>
+                <Link to="/pages/resources" className="text-muted u px-2">Tools &amp; Resources</Link>
+                <Link to="/reviews" className="text-muted u px-2">Reviews</Link>
             </div>
             <div className="my-2">
-                <span className="strong pr-1">Cheat sheets:</span>
-                <Link className="text-muted u px-1" to="/blog/htmlcss-cheatsheet">HTML &amp; CSS Cheatsheet</Link>
-                <Link className="text-muted u px-1" to="/blog/js-cheatsheet">JavaScript Cheatsheet</Link>
+                <span className="strong pr-1">Cheat sheets</span>
+                <Link className="text-muted u px-2" to="/blog/htmlcss-cheatsheet">HTML &amp; CSS Cheatsheet</Link>
+                <Link className="text-muted u px-2" to="/blog/js-cheatsheet">JavaScript Cheatsheet</Link>
             </div>
             <div className="my-2">
                 <span className="strong pr-1">Company</span>
-                <Link to="/blog" className="text-muted u px-1">Blog</Link>
-                <Link to="/pages/about" className="text-muted u px-1">About us</Link>
-                <Link to="/pages/affiliates" className="text-muted u px-1">Affiliates</Link>
-                <a className="text-muted u px-1" href="mailto:support@ilovecoding.org">support@ilovecoding.org</a>
+                <Link to="/blog" className="text-muted u px-2">Blog</Link>
+                <Link to="/pages/about" className="text-muted u px-2">About us</Link>
+                <Link to="/pages/affiliates" className="text-muted u px-2">Affiliates</Link>
+                <a className="text-muted u px-2" href="mailto:support@ilovecoding.org">support@ilovecoding.org</a>
             </div>
             <div className="my-2">
                 <span className="strong pr-1">Social</span>
-                <a className="text-muted u px-1" href="https://www.facebook.com/pg/ilovecodingorg">Facebook</a>
-                <a className="text-muted u px-1" href="https://www.instagram.com/ilovecoding/">Instagram</a>
-                <a className="text-muted u px-1" href="https://twitter.com/ilovecodingorg">Twitter</a>
-                <a className="text-muted u px-1" href="https://www.quora.com/profile/Aziz-Ali">Quora</a>
+                <a className="text-muted u px-2" href="https://www.facebook.com/pg/ilovecodingorg">Facebook</a>
+                <a className="text-muted u px-2" href="https://www.instagram.com/ilovecoding/">Instagram</a>
+                <a className="text-muted u px-2" href="https://twitter.com/ilovecodingorg">Twitter</a>
+                <a className="text-muted u px-2" href="https://www.quora.com/profile/Aziz-Ali">Quora</a>
             </div>
             <Copyrights />
         </div>
